eef6f4c6e2b8dc640350d705d3c0b4a9154f9504
Pothagani Naveen Prasad <97022233+pnaveeen-yadav@users.noreply.github.com>
2026-09-04 16:22:09 +0530

    updaetd css

--- sree-krishna-housing-projects-github/app/visit/page.tsx
+++ sree-krishna-housing-projects-github/app/visit/page.tsx
@@ -4,14 +4,28 @@ import SiteVisitForm from "../components/SiteVisitForm";
 export default function VisitPage() {
   return (
     <main className="visitPage">
-      {/* HEADER */}
-      <header className="visitNav">
+      {/* HEADER - SAME AS HOME PAGE */}
+      <header className="nav visitNav">
+        <Link href="/" className="brand">
+          <span>SK</span>
+
+          <div>
+            <b>SREE KRISHNA</b>
+            <small>HOUSING PROJECTS</small>
+          </div>
+        </Link>
+
         <nav className="visitNavLinks">
           <Link href="/">Home</Link>
           <Link href="/properties">Properties</Link>
           <Link href="/#services">Services</Link>
+          <Link href="/#testimonials">Testimonials</Link>
           <Link href="/#contact">Contact</Link>
         </nav>
+
+        <Link href="/visit" className="btn gold">
+          Book Site Visit
+        </Link>
       </header>
 
       {/* PAGE CONTENT */}
