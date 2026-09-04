25ca669207aa9a0d8c05505238e124f3240d45a4
Pothagani Naveen Prasad <97022233+pnaveeen-yadav@users.noreply.github.com>
2026-09-04 15:17:44 +0530

    updated css

--- sree-krishna-housing-projects-github/app/visit/page.tsx
+++ sree-krishna-housing-projects-github/app/visit/page.tsx
@@ -5,34 +5,17 @@ export default function VisitPage() {
   return (
     <main className="visitPage">
       {/* HEADER */}
-      <header className="nav visitNav">
-        <Link href="/" className="brand">
-          <span>SK</span>
-
-          <div>
-            <b>SREE KRISHNA</b>
-            <small>HOUSING PROJECTS</small>
-          </div>
-        </Link>
-
-        <nav>
+      <header className="visitNav">
+        <nav className="visitNavLinks">
           <Link href="/">Home</Link>
-
           <Link href="/properties">Properties</Link>
-
           <Link href="/#services">Services</Link>
-
           <Link href="/#testimonials">Testimonials</Link>
-
           <Link href="/#contact">Contact</Link>
         </nav>
-
-        <Link href="/visit" className="btn dark">
-          Book Site Visit
-        </Link>
       </header>
 
-      {/* VISIT CONTENT */}
+      {/* PAGE CONTENT */}
       <section className="visitPageContent">
         <h1>Schedule Your Visit</h1>
 
