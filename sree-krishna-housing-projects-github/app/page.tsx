49832d295234c38d41719dd059808a04387ecec6
Pothagani Naveen Prasad <naveenyadav36090@gmail.com>
2026-09-12 01:48:28 +0530

    Add Link component import for enhanced navigation functionality

--- sree-krishna-housing-projects-github/app/page.tsx
+++ sree-krishna-housing-projects-github/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import SiteVisitForm from "./components/SiteVisitForm";
 import { supabase } from "@/lib/supabase";
 
